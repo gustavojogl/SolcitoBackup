aa408160b9b82104c83c9e11826c0f31853e113a
Gustavo Sanchez <joglabs@gmail.com>
2017-08-09 14:36:31 -0300

    Cron

--- cron.php
+++ cron.php
@@ -1,3 +1,58 @@
+<?php /**
+            * PHPMyLicense Check Class v2.5.0
+            *
+            * PHP version > 5
+            *
+            * LICENSE: This source file is subject to version 3.01 of the PHP license
+            * that is available through the world-wide-web at the following URI:
+            * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+            * the PHP License and are unable to obtain it through the web, please
+            * send a note to license@php.net so we can mail you a copy immediately.
+            *
+            * @package    PHPLicencia
+            * @author     IDIBox <idiboxlac@gmail.com>
+            * @copyright  2017 PHPLicencia
+            * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+            * @version    dev-2017
+            * @link       https://phplicencia.com */
+            
+            $domain=$_SERVER['SERVER_NAME'];
+            $product="8";
+            $licenseServer = "http://agarrado.idibox.com/api/";
+
+            $postvalue="domain=$domain&product=".urlencode($product);
+
+            $ch = curl_init();
+            curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_URL, $licenseServer);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postvalue);
+            $result= json_decode(curl_exec($ch), true);
+            curl_close($ch);
+
+            if($result['status'] != 200) {
+            $html = "<div align='center'>
+    <table width='100%' border='0' style='padding:15px; border-color:#F00; border-style:solid; background-color:#FF6C70; font-family:Tahoma, Geneva, sans-serif; font-size:22px; color:white;'>
+
+    <tr>
+
+        <td><b>You don't have permission to use this product. The message from server is: <%returnmessage%> <br > Contact the product developer.</b></td >
+
+    </tr>
+
+    </table>
+
+</div>";
+            $search = '<%returnmessage%>';
+            $replace = $result['message'];
+            $html = str_replace($search, $replace, $html);
+
+
+            die( $html );
+
+            }
+            ?>
+
 <?php
 @ini_set('log_errors', 1);
 @ini_set('display_errors', 0);
